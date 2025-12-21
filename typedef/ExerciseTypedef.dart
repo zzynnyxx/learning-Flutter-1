2a17396b35fad978d8317e738e26813f2e8e982a
Zzynnyxx <collinsmwenda888@gmail.com>
2025-12-21 11:44:16 +0300

    changess on the ExerciseTypedef

---
+++ typedef/ExerciseTypedef.dart
@@ -0,0 +1,47 @@
+/* 
+Exercise for you
+
+Create a typedef called YearList for a List<int> representing birth years.
+
+Create a typedef called AgeCalculator for a function that takes a birth year (int) and returns an age (int).
+
+Write a program that:
+
+Creates a list of birth years.
+
+Uses a function (with the typedef) to calculate the ages.
+
+Prints the list of ages. 
+*/
+
+
+
+typedef YearList = List<int>;
+typedef AgeCalculator = int Function(int year);
+
+
+void main(){
+  // representing Birthyears
+  YearList Birthyears =[2003,2009,2005];
+   
+
+
+   // calculating age
+   AgeCalculator currentAge=(year) => 2025-year;
+
+
+   // storing age 
+   List <int > ages = [];
+
+   //adding agesto list
+
+   for(int year in Birthyears){
+    ages.add(currentAge(year));
+   }
+
+
+   print('The birth years are :$Birthyears');
+   print("The corresponding  ages is : $ages");
+
+
+}
