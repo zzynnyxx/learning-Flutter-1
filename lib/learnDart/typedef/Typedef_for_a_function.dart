cf18c1bd166fa4b1d4ac9290677a32f0decd140f
Zzynnyxx <collinsmwenda888@gmail.com>
2025-12-22 13:23:13 +0300

    changes to typedef ue to error

--- lib/learnDart/typedef/Typedef_for_a_function.dart
+++ lib/learnDart/typedef/Typedef_for_a_function.dart
@@ -1,37 +1,37 @@
-// typedef for shopping cart and srting function
+// // typedef for shopping cart and srting function
 
-import 'package:learn_flutter/learnDart/typedef/typedef.dart';
+// import 'package:learn_flutter/learnDart/typedef/typedef.dart';
 
-typedef ShoppingCart = Map<String, List<String>>;
-typedef Sorter <T> = int Function (T a , T b);
-
-
-
-void main(){
-
-  // Shopping cart : usr -> List of items
-
-  ShoppingCart cart ={
-    'Alice ' : ['apple', 'banana', 'kiwi'],
-    'bob': ['milk', 'bread','eggs']
-  };
-
-
-  // sorting function : sort items by lenght of name 
-  Sorter <String> sortByLength =(a, b) => a.length - b.length ;
-
-  print('Before sorting : $cart');
-
-
-  //sort each user's items
-  cart.forEach((user, items){
- items.sort(sortByLength);
-  });
+// typedef ShoppingCart = Map<String, List<String>>;
+// typedef Sorter <T> = int Function (T a , T b);
 
 
 
-  print("After sorting: $cart");
+// void main(){
+
+//   // Shopping cart : usr -> List of items
+
+//   ShoppingCart cart ={
+//     'Alice ' : ['apple', 'banana', 'kiwi'],
+//     'bob': ['milk', 'bread','eggs']
+//   };
+
+
+//   // sorting function : sort items by lenght of name 
+//   sortByLength(a, b) => a.length - b.length ;
+
+//   print('Before sorting : $cart');
+
+
+//   //sort each user's items
+//   cart.forEach((user, items){
+//  items.sort(sortByLength);
+//   });
+
+
+
+//   print("After sorting: $cart");
   
   
   
-  }
+//   }
